cda0e4260aa9b6d53005210c495cedde912c5705
hoang-debug <campus199@gmail.com>
2022-02-17 23:49:20 +0700

    CQRS

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,29 +1,37 @@
+using MediatR;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
+        // private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
+        // public ActivitiesController(IMediator mediator)
+        // {
+        //     _mediator = mediator;
+        // }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Detail.Query{Id=id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{Activity=activity}));
         }
     }
 }
